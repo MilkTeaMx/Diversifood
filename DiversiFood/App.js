54ce804a3610bdc16dad7f10fd970e843142a048
backpack455 <39465356+backpack455@users.noreply.github.com>
2022-04-18 18:59:32 -0400

    Big Brain Joshi Strike Again

--- DiversiFood/App.js
+++ DiversiFood/App.js
@@ -1,20 +1,227 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  FontAwesome5,
+} from "@expo/vector-icons";
 
-export default function App() {
+import HomeScreen from "./screens/AuthScreens/HomeScreen";
+import SplashScreen from "./screens/AuthScreens/SplashScreen";
+import LoginScreen from "./screens/AuthScreens/LoginScreen";
+import RegisterScreen from "./screens/AuthScreens/RegisterScreen";
+import IngredientsScreen from './screens/MainScreens/IngredientsScreen'
+import Firebasekeys from "./config";
+// import * as firebase from "firebase";
+// import "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged} from "firebase/auth";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+let firebaseConfig = Firebasekeys;
+
+if (!firebase.apps.length) {
+  const firebaseApp = initializeApp(firebaseConfig);
+}
+
+const inactiveColor = '#8E8E8E'
+const themecolor = '#357342'
+const tabcolor = '#fff'
+const Tab = createMaterialBottomTabNavigator();
+const Auth = createStackNavigator();
+const Home = createStackNavigator();
+const Ingredients = createStackNavigator();
+
+const IngredientsScreenNavigator = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home.Navigator
+      screenOptions={{
+          headerShown:false
+        }}
+      
+      initialRouteName="Ingredients"
+    >
+      <Home.Screen name="Ingredients Screen" component={IngredientsScreen}
+      options={{
+        title: 'Home',
+        headerShown: true,
+          headerStyle: {
+            backgroundColor: `${themecolor}`
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff'
+          },
+          headerBackTitleStyle: {
+            color: `${inactiveColor}`
+          },
+          headerTintColor: `${inactiveColor}`,
+        }}
+       />
+      <Home.Screen name="Pollen Heatmap" component={IngredientsScreen} options={{
+          title: 'Heatmap',
+          headerShown: true,
+            headerStyle: {
+              backgroundColor: `${themecolor}`
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: '#fff'
+            },
+            headerBackTitleStyle: {
+              color: `${inactiveColor}`
+            },
+            headerTintColor: `${inactiveColor}`,
+          }}/>
+    </Home.Navigator>
+  );
+};
+
+const HomeScreenNavigator = ({ navigation }) => {
+  return (
+    <Home.Navigator
+      screenOptions={{
+          headerShown:false
+        }}
+      
+      initialRouteName="Home Screen"
+    >
+      <Home.Screen name="Home Screen" component={HomeScreen}
+      options={{
+        title: 'Home',
+        headerShown: true,
+          headerStyle: {
+            backgroundColor: `${themecolor}`
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: '#fff'
+          },
+          headerBackTitleStyle: {
+            color: `${inactiveColor}`
+          },
+          headerTintColor: `${inactiveColor}`,
+        }}
+       />
+      <Home.Screen name="Pollen Heatmap" component={IngredientsScreen} options={{
+          title: 'Heatmap',
+          headerShown: true,
+            headerStyle: {
+              backgroundColor: `${themecolor}`
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: '#fff'
+            },
+            headerBackTitleStyle: {
+              color: `${inactiveColor}`
+            },
+            headerTintColor: `${inactiveColor}`,
+          }}/>
+    </Home.Navigator>
+  );
+};
+
+function MainTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Dashboard"
+        sceneAnimationEnabled="true"
+        activeColor={tabcolor}
+        inactiveColor={inactiveColor}
+        barStyle={{ backgroundColor: `${themecolor}`, bottomPadding: 10 }}
+        shifting={true}
+      >
+        <Tab.Screen
+          name="Dashboard"
+          component={HomeScreenNavigator}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <MaterialCommunityIcons
+                name="view-dashboard"
+                size={26}
+                color={focused ? tabcolor : inactiveColor}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Ingredients Screen"
+          component={IngredientsScreenNavigator}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Entypo
+                name="bell"
+                size={24}
+                color={focused ? tabcolor : inactiveColor}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+function AuthNavigator() {
+  return (
+    <NavigationContainer>
+      <Auth.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Auth.Screen name="Splash" component={SplashScreen} options={{}} />
+        <Auth.Screen name="Login" component={LoginScreen} options={{}} />
+        <Auth.Screen name="Register" component={RegisterScreen} options={{}} />
+        {/* <Auth.Screen name="MainTabs" component={MainTabs}
+       options={{
+         headerTitle: "Complaint Form Submission",
+       }}
+    /> */}
+      </Auth.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App2() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(); // Handle user state changes
+  const [app, setApp] = useState();
+  const [subscriber, setSubscriber] =  useState();
+
+  function onAuthStateChangedCentral(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      const firebaseApp = initializeApp(firebaseConfig);
+      setApp(firebaseApp)
+    }
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, user=>{
+      const sub = onAuthStateChangedCentral(user)
+      setSubscriber(sub)
+    });
+    return subscriber
+  }, [])
+  if (initializing) return null;
+  console.log(user)
+  if (!user) {
+    return <AuthNavigator />;
+  }
+
+  return <MainTabs />;
+}
---
+++ DiversiFood/config.js
@@ -0,0 +1,10 @@
+export default firebasekeys = {
+    apiKey: "AIzaSyAUsDdRz5V7QcbFnrhjvfaa24a9jyjzjWo",
+    authDomain: "diversifood-412fe.firebaseapp.com",
+    projectId: "diversifood-412fe",
+    storageBucket: "diversifood-412fe.appspot.com",
+    messagingSenderId: "611099564946",
+    appId: "1:611099564946:web:e3aa1315e42f275de63f4b",
+    measurementId: "G-PHK1L0JZ8Y"
+  };
+  
